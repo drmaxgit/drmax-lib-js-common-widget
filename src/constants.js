847f7e2eaa0e39f63b258014b1080d726a938234
Erik Brožek <erik@brozek.name>
2019-06-05 08:47:03 +0200

    Changed: format code, plus added sso, checkout, search, sukl widget DRMAX-4945

--- src/constants.js
+++ src/constants.js
@@ -1,28 +1,50 @@
 // DEFAULT CONSTANTS
-export const DRMAX_ESHOP_API_URL = 'https://live-api-eshop.drmax.cz'
-export const DRMAX_ESHOP_WEB_URL = 'https://www.drmax.cz'
-export const DRMAX_CHECKOUT_WEB_URL = 'https://kosik.drmax.cz'
-export const DRMAX_CHECKOUT_REFRESHDELAY = 30
-export const DRMAX_COMMON_DATALAYER_NAME = 'dataLayerDRMAX'
-export const DRMAX_COMMON_COOKIE_DOMAIN = '.drmax.cz'
-export const DRMAX_COMMON_COOKIE_MAXAGE = 2592000
+
+// Apotheka
 export const DRMAX_APOTHEKA_API_URL = 'https://live-api-apotheka.drmax.cz'
+export const DRMAX_APOTHEKA_API_URL_PREFIX = '/api/v1/pharmacy'
 export const DRMAX_APOTHEKA_WEB_URL = 'https://lekarny.drmax.cz'
-export const DRMAX_SEARCH_API_URL = 'https://live-api-algolia.drmax.cz'
-export const DRMAX_SSO_WEB_URL = 'https://sso.drmax.cz'
-export const DRMAX_CMS_API_URL = 'https://www.drmax.cz'
-export const DRMAX_MEDIA_WEB_URL = 'https://media-eshop.drmax.cz'
-export const DRMAX_COMMON_WEB_URL = 'https://live-common.drmax.cz'
-export const DRMAX_MEGAMENU_WIDGET_URL = 'https://live-widget-megamenu.drmax.cz'
 export const DRMAX_APOTHEKA_WIDGET_URL = 'https://live-widget-apotheka.drmax.cz'
 
-// APOTHEKA
-export const DRMAX_APOTHEKA_API_URL_PREFIX = '/api/v1/pharmacy'
+// Checkout
+export const DRMAX_CHECKOUT_REFRESHDELAY = 30
+export const DRMAX_CHECKOUT_WEB_URL = 'https://kosik.drmax.cz'
+export const DRMAX_CHECKOUT_WIDGET_URL = 'httAps://live-widget-kosik.drmax.cz' // new
 
-// LOGNI
+// CMS
+export const DRMAX_CMS_API_URL = 'https://www.drmax.cz'
+
+// Common
+export const DRMAX_COMMON_COOKIE_DOMAIN = '.drmax.cz'
+export const DRMAX_COMMON_COOKIE_MAXAGE = 2592000
+export const DRMAX_COMMON_DATALAYER_NAME = 'dataLayerDRMAX'
+export const DRMAX_COMMON_WEB_URL = 'https://live-common.drmax.cz'
+
+// Eshop
+export const DRMAX_ESHOP_API_URL = 'https://live-api-eshop.drmax.cz'
+export const DRMAX_ESHOP_MEDIA_URL = 'https://media-eshop.drmax.cz' // new eshop media
+export const DRMAX_MEDIA_WEB_URL = 'https://media-eshop.drmax.cz' // old eshop media
+export const DRMAX_ESHOP_WEB_URL = 'https://www.drmax.cz'
+
+// Logni (https://github.com/erikni/logni.js)
 export const LOGNI_MASK = window.logniMask || 'I3E1C1W2'
 export const LOGNI_LOG_URL = window.logniLogUrl || 'https://live-jslog.logni.net'
-export const LOGNI_STD_ERR = window.logniStdErr || '0'
+export const LOGNI_STD_ERR = window.logniStdErr || 0 // old
+export const LOGNI_STDERR = window.logniStdErr || 0 // new
 export const LOGNI_RELEASE = window.logniRelease || '0.0.0'
 export const LOGNI_ENVIROMENT = window.logniEnvironment || 'unknown'
 export const LOGNI_NAME = window.logniName || 'drmax-widget'
+
+// Mega menu
+export const DRMAX_MEGAMENU_WIDGET_URL = 'https://live-widget-megamenu.drmax.cz'
+
+// Search
+export const DRMAX_SEARCH_API_URL = 'https://live-api-algolia.drmax.cz'
+export const DRMAX_SEARCH_WIDGET_URL = 'https://live-widget-searchsuggest.drmax.cz' // new
+
+// SSO
+export const DRMAX_SSO_WEB_URL = 'https://sso.drmax.cz'
+export const DRMAX_SSO_WIDGET_URL = 'https://live-widget-sso.drmax.cz' // new
+
+// SUKL
+export const DRMAX_SUKL_WIDGET_URL = 'https://live-widget-sukl.drmax.cz' // new
